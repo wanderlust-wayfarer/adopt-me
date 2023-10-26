4c31bc05973f3c0d82ba37d9da0bb6a0ac0710f6
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-25 22:16:52 -0700

    modernized JSX format & expanded linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Kitsune",
-      breed: "Husky",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Fern",
-      breed: "American Shorthair",
-    }),
-    React.createElement(Pet, {
-      animal: "Axolotl",
-      name: "Potato",
-      breed: "Leucistic",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet animal="Dog" name="Kitsune" breed="Husky" />
+      <Pet animal="Cat" name="Fern" breed="American Shorthair" />
+      <Pet animal="Axolotl" name="Potato" breed="Leucistic" />
+    </div>
+  )
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
