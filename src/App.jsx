3e453231692feed5e4e77d61ca7f7c2884edc15f
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-25 21:43:38 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Kitsune",
+      breed: "Husky",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Fern",
+      breed: "American Shorthair",
+    }),
+    React.createElement(Pet, {
+      animal: "Axolotl",
+      name: "Potato",
+      breed: "Leucistic",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
