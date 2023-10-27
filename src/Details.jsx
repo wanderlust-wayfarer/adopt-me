9359ba6cdaaab2f3bf0117a1b4e7f2ac7e8e7e5e
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-27 14:34:37 -0700

    refactoring fetchBreedList

--- src/Details.jsx
+++ src/Details.jsx
@@ -25,7 +25,7 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>
-          {`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
           <button>Adopt {pet.name}</button>
           <p>{pet.description}</p>
         </h2>
