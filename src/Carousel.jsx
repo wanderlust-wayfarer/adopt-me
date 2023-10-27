477de5b92deaa3627fd8ad2d273aed04fb7f826f
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-27 15:43:03 -0700

    handling events in class components

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,22 +9,29 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
-
-    console.info(images);
 
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
-              src={photo}
-              className={index === active ? "active" : ""}
               alt="animal thumbnail"
+              className={index === active ? "active" : ""}
+              data-index={index}
+              src={photo}
+              onClick={this.handleIndexClick}
             />
           ))}
         </div>
