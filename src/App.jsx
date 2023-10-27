7cde0a96bcf9af8b9c15b0e594bcf9ae09def63b
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-27 14:23:02 -0700

    performance optimization with React Query

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
-import Details from "../Details";
+import Details from "./Details";
 
 const queryClient = new QueryClient({
   defaultOptions: {
