cb5ffb56d98ea4d32b52750d8e982fbf613789c9
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-10-27 12:11:10 -0700

    React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "../Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      startTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
