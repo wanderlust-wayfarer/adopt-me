96e71dbba6c0008855823538f45e57a6944fd875
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-11-15 22:05:46 -0800

    typing component with react query

--- src/useBreedList.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import fetchBreedList from "./fetchBreedList";
-
-function useBreedList(animal) {
-  const results = useQuery(["breeds", animal], fetchBreedList);
-  return [results?.data?.breeds ?? [], results.status];
-}
-
-export default useBreedList;
---
+++ src/useBreedList.ts
@@ -0,0 +1,13 @@
+import { QueryStatus, useQuery } from "@tanstack/react-query";
+import { Animal } from "./APIResponsesTypes";
+import fetchBreedList from "./fetchBreedList";
+
+function useBreedList(animal: Animal) {
+  const results = useQuery(["breeds", animal], fetchBreedList);
+  return [results?.data?.breeds ?? [], results.status] as [
+    string[],
+    QueryStatus,
+  ];
+}
+
+export default useBreedList;
